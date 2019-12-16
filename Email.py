9119a39dd7658eaa273efb6bd4001bb200f468b3
lsxyq <39256884+lsxyq@users.noreply.github.com>
2019-12-16 11:56:44 +0800

    Update Email.py

--- Email.py
+++ Email.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-# Author:Leslie-x 
-
+# -*- coding:UTF-8 -*-
+# Author:Leslie-x
 import smtplib, time, os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -10,168 +9,139 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 
-class Settings():
-    def __init__(self, username, password, subject, server, sender, receiver):
-        self.server = server
-        self.sender = sender
-        self.receiver = receiver
-        self.username = username
-        self.password = password
-        self.subject = subject
-        # 通过Header对象编码的文本，包含utf-8编码信息和Base64编码信息。以下中文名测试ok
-        # subject = '中文标题'
-        # subject=Header(subject, 'utf-8').encode()
-
-
 class Message():
     # 构造邮件对象MIMEMultipart对象
     _msg = MIMEMultipart('mixed')
 
-    def __init__(self, set, content):
-        # 下面的主题，发件人，收件人，日期是显示在邮件页面上的。
-        self._msg['Subject'] = set.subject
-        self._msg['From'] = set.username
-        # 收件人为多个收件人,通过join将列表转换为以;为间隔的字符串
-        self._msg['To'] = ";".join(set.receiver)
-        self._msg['Date'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        self.content = content
+    def __init__(self):
+        pass
 
-    def get_msg(self):
-        for info in self.content.get_content():
+    @classmethod
+    def get_text(cls, text):
+        """
+        :param text:
+            eg :Hello！\nHow are you?\nHere is the link you wanted:\nhttp://www.baidu.com
+        :return:
+        """
+        if not text:
+            return None
+        text_plain = MIMEText(text, 'plain', 'utf-8')
+        return text_plain
+
+    @classmethod
+    def get_image(cls, image_path):
+        """
+        :param image_path:
+            eg: F:\a\b\c.jpg
+        :return:
+        """
+        if not os.path.exists(image_path):
+            raise FileNotFoundError("Not Find This File")
+        filename = os.path.basename(image_path)
+        image_data = open(r'%s' % image_path, 'rb').read()
+        image = MIMEImage(image_data)
+        image.add_header('Content-Disposition', 'attachment', filename=('gbk', '', filename))
+        return image
+
+    @classmethod
+    def get_html(cls, html):
+        """
+        :param html:
+            <html>
+              <head></head>
+              <body>
+                <p>Hi!<br>
+                   How are you?<br>
+                   Here is the <a href="http://www.baidu.com">link</a> you wanted.<br>
+                </p>
+              </body>
+            </html>
+        :return:
+        """
+        if not html:
+            return None
+        text_html = MIMEText(html, 'html', 'utf-8')
+        text_html["Content-Disposition"] = 'attachment; filename="TEXT/HTML.html"'
+        return text_html
+
+    @classmethod
+    def get_att(cls, att_path):
+        """
+        :param att_path:
+            eg:F:\Python开发工程师测试题（一）.docx
+        :return:
+        """
+        if not os.path.exists(att_path):
+            raise FileNotFoundError("Not Find This File")
+        filename = os.path.basename(att_path)
+        att = MIMEBase('application', 'octet-stream')
+        att.set_payload(open(att_path, 'rb').read())
+        att.add_header('Content-Disposition', 'attachment', filename=('gbk', '', filename))
+        encoders.encode_base64(att)
+        return att
+
+    def get_msg(self, text=None, html=None, image_path=None, att_path=None):
+        info_list = []
+        info_list.append(Message.get_text(text))
+        info_list.append(Message.get_html(html))
+        info_list.append(Message.get_att(att_path))
+        info_list.append(Message.get_image(image_path))
+        for info in info_list:
             if not info: continue
             self._msg.attach(info)
         return self._msg
 
 
-class EmailContent():
-    def __init__(self, text=None, html=None, image_path=None, att_path=None):
-        self.text = text
-        self.image = image_path
-        self.html = html
-        self.att_path = att_path
-
-    def get_filename(self, path):
-        import os
-        return os.path.basename(path)
-
-    @property
-    def get_text(self):
-        # 构造文本
-        if not self.text:
-            return None
-        self.text_plain = MIMEText(self.text, 'plain', 'utf-8')
-        return self.text_plain
-
-    @property
-    def get_image(self):
-        # 构造图片链接
-        if not self.image:
-            return None
-        if not os.path.exists(self.image):
-            raise FileNotFoundError("Not Find This File")
-        filename = self.get_filename(self.image)
-        self.sendimagefile = open(r'%s' % self.image, 'rb').read()
-        self.image = MIMEImage(self.sendimagefile)
-        self.image.add_header('Content-Disposition', 'attachment', filename=('gbk', '', filename))
-        return self.image
-
-    @property
-    def get_html(self):
-        # 构造html
-        if not self.html:
-            return None
-        self.text_html = MIMEText(self.html, 'html', 'utf-8')
-        self.text_html["Content-Disposition"] = 'attachment; filename="TEXT/HTML.html"'
-        return self.text_html
-
-    @property
-    def get_att(self):
-        "# 构造附件"
-        if not self.att_path:
-            return None
-        if not os.path.exists(self.att_path):
-            raise FileNotFoundError("Not Find This File")
-        filename = self.get_filename(self.att_path)
-        self.att = MIMEBase('application', 'octet-stream')
-        self.att.set_payload(open(self.att_path, 'rb').read())
-        self.att.add_header('Content-Disposition', 'attachment', filename=('gbk', '', filename))
-        encoders.encode_base64(self.att)
-        return self.att
-
-    def get_content(self):
-        return self.get_text, self.get_image, self.get_html, self.get_att
-
-
-class Smtp():
+class SmtpServer():
     _smtp = smtplib.SMTP()
 
-    def __init__(self, set, msg):
-        self.set = set
-        self.msg = msg
+    def __init__(self, config):
+        self.config = config
 
-    def _connect(self):
-        # 我们用set_debuglevel(1)就可以打印出和SMTP服务器交互的所有信息。
-        # smtp.set_debuglevel(1)
-        self._smtp.connect(self.set.server)
+    def __getattr__(self, item):
+        value = self.config.get(item, None)
+        if not value:
+            raise AttributeError("object has no attribute '%s'" % item)
+        return value
 
-    def _login(self):
-        self._smtp.login(self.set.username, self.set.password)
+    def message_set(self, msg):
+        """
+        下面的主题，发件人，收件人，日期是显示在邮件页面上的。
+        通过Header对象编码的文本，包含utf-8编码信息和Base64编码信息。以下中文名测试ok
+        subject = '中文标题'
+        subject=Header(subject, 'utf-8').encode()
+        收件人为多个收件人,通过join将列表转换为以;为间隔的字符串
+        """
+        msg['From'] = self.username
+        msg['Subject'] = self.subject
+        msg['To'] = ";".join(self.receiver)
+        msg['Date'] = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        return msg
 
-    def _sendmail(self):
-        self._smtp.sendmail(self.set.sender, self.set.receiver, self.msg.as_string())
-
-    def _quit(self):
+    def send_email(self, msg: MIMEMultipart):
+        email = self.message_set(msg)
+        self._smtp.connect(self.server)
+        self._smtp.login(
+            user=self.username,
+            password=self.password
+        )
+        self._smtp.sendmail(
+            from_addr=self.sender,
+            to_addrs=self.receiver,
+            msg=email.as_string()
+        )
         self._smtp.quit()
 
-    def start(self):
-        self._connect()
-        self._login()
-        self._sendmail()
-        self._quit()
-
-
-"""
-可选参数
-text 邮件正文内容（文本）
-html 邮件附件内容（HTML）
-image_path 邮件附件内容（图片路径）
-att_path 邮件附件内容（文件路径）
-"""
-text = "Hi!你好啊！！\nHow are you?\nHere is the link you wanted:\nhttp://www.baidu.com"
-html = """
-<html>  
-  <head></head>  
-  <body>  
-    <p>Hi!<br>  
-       How are you?<br>  
-       Here is the <a href="http://www.baidu.com">link</a> you wanted.<br> 
-    </p> 
-  </body>  
-</html>  
-"""
-image_path = "F:\爬虫\IMGS\高桥圣子1.jpg"
-att_path = r'F:\Python开发工程师测试题（一）.docx'
-
-"""
-必要参数
-username  邮箱登陆账号
-password  邮箱密码
-subject   邮件主题
-server    邮件服务器
-sender    发送方
-receiver  收件人（列表格式，多个收件人）
-"""
-username = "xxxx9.com"
-password = 'xxxx
-subject = '测试邮件'
-server = 'smtp.139.com'
-sender = 'xxx@139.com'
-receiver = ['xxx@qq.com']
 
 if __name__ == '__main__':
-    set = Settings(username, password, subject, server, sender, receiver)
-    content = EmailContent(text=text, image_path=image_path, att_path=att_path, html=html)
-    message = Message(set, content)
-    msg = message.get_msg()
-    smtp = Smtp(set, msg)
-    smtp.start()
+    config = {
+        'username': "xxxx9.com",
+        "password": "xxxx",
+        "subject": '测试邮件',
+        "server": "smtp.139.com",
+        "sender": "xxx@139.com",
+        "receiver": ['xxx@qq.com']
+    }
+    t = r'Hello！\nHow are you?\nHere is the link you wanted:\nhttp://www.baidu.com'
+    msg = Message().get_msg(text=t)
+    SmtpServer(config).send_email(msg)
